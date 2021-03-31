919a021c076d84e814b02c5043f63ae77cb80fa2
Camilo Souza <kmilosouza@gmail.com>
2021-03-30 21:45:40 -0400

    Second commit. Codes related to blogg development

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello world')
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+def getDbConnection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row  
+    return conn
+
+@app.route('/')
+def hello():
+    conn = getDbConnection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
